c688d601d83137c46a8f4ec067460aaf485787fa
saranshahuja <saranshahuja614@gmail.com>
2023-07-16 17:47:56 -0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:timesync/firebase_options.dart';
 
 import 'Pages/HomeScreen.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(ProviderScope(child: MyApp()));
 }
 
